494606d67bd5a0671d9c5ba13f9fe3ac71a238f3
Matt Iggo <matt@iggo.co.uk>
2019-07-12 00:55:06 +0100

    Major update
    Added console logging
    Added capability to retry requests
    Reduced timeout per request to 10 seconds from default
    Set 10 retries per request; If 10 fail the deduplication fails with an exception
    Added some tests to the response in order to get around things like:
    
    org.archive.wayback.exception.RobotAccessControlException: Blocked By Robots
    <title>Bad Request</title>
    </head>
     <body>
    <h1><p>Bad Request</p></h1>
     Request Line is too large (8192 &gt; 4094)
    </body>
    </html>
    
    The latter 2 are ignored and treated as "not seen", So they will likely duplicate. This is typically only a problem for malformed JS where the wpull code renderer goes a bit awol.
    
    Implemented django URL validation to verify the URLs coming back from the API are valid.

--- warc_dedup/deduplicate.py
+++ warc_dedup/deduplicate.py
@@ -1,8 +1,14 @@
+# encoding=utf8
 import datetime
 import os
-
 import requests
-import warcio
+import re
+import random
+import time
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from django.conf import settings as djangosettings
+
 from warcio.archiveiterator import ArchiveIterator
 from warcio.warcwriter import WARCWriter
 
@@ -10,19 +16,21 @@ from warc_dedup.log import Log
 
 
 class Warc:
-    def __init__(self, warc_source: str, warc_target: str=None):
+    def __init__(self, warc_source: str, warc_target: str = None):
         self.warc_source = warc_source
         self.warc_target = warc_target
         self._response_records = {}
         self._log = Log()
         self._log.log('Original WARC file is {}.'.format(self.warc_source))
         self._log.log('Deduplicated WARC file is {}.'.format(self.warc_target))
+        djangosettings.configure(DEBUG=False)
         if os.path.isfile(self.warc_target):
             self._log.log('File {} already exists.'.format(self.warc_target))
             raise Exception('File {} already exists.'.format(self.warc_target))
 
     def deduplicate(self):
         self._log.log('Start deduplication process.')
+        print('Start deduplication process.')
         with open(self.warc_source, 'rb') as s, \
                 open(self.warc_target, 'wb') as t:
             writer = WARCWriter(filebuf=t, gzip=self.warc_target.endswith('.gz'))
@@ -40,6 +48,7 @@ class Warc:
                     if data:
                         self._log.log('Record {} is duplicate from {}.'
                                       .format(record_id, data))
+                        print('URL {} is a duplicate - Writing record'.format(url))
                         writer.write_record(
                             self.response_to_revisit(writer, record, data)
                         )
@@ -99,24 +108,67 @@ class Warc:
 
     @staticmethod
     def get_ia_duplicate(record):
+        tries = 0
+        delay = 0
+        url_re = re.compile('[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
         date = record.rec_headers.get_header('WARC-Date')
         date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
         date = date.strftime('%Y%m%d%H%M%S')
         digest = record.rec_headers.get_header('WARC-Payload-Digest')
         uri = record.rec_headers.get_header('WARC-Target-URI')
-        r = requests.get(
-            'http://wwwb-dedup.us.archive.org:8083/cdx/search'
-            '?url={}'.format(uri) +
-            '&limit=1'
-            '&filter=digest:{}'.format(digest.split(':')[1]) +
-            '&fl=original,timestamp'
-            '&to={}'.format(int(date) - 1) +
-            '&filter=!mimetype:warc\/revisit'
-        )
+        print('Processing URL {}.'.format(uri))
+        while True:
+            try:
+                if tries <= 9:
+                    pass
+                else:
+                    raise Exception('Internet Archive Deduplication CDX API Offline - Aborting')
+                tries += 1
+                r = requests.get(
+                    'http://wwwb-dedup.us.archive.org:8083/cdx/search/'
+                    '?url={}'.format(uri) +
+                    '&limit=1'
+                    '&filter=digest:{}'.format(digest.split(':')[1]) +
+                    '&fl=original,timestamp'
+                    '&to={}'.format(int(date) - 1) +
+                    '&filter=!mimetype:warc\/revisit',
+                    timeout = 3
+                )
+            except requests.exceptions.ConnectionError or requests.exceptions.ReadTimeout or requests.exceptions.Timeout as error:
+                print("Error connecting to wwwb-dedupe.us.archive.org/cdx/search - Sleeping for " + str(round(delay, 2)) \
+                     + " seconds.")
+                time.sleep(delay)
+                delay += delay + (random.randint(0,1000)/1000)
+                continue
+            break
         r = r.text.strip()
         if len(r) == 0:
             return None
         r = r.split(' ', 1)
+        if 'org.archive.wayback.exception.RobotAccessControlException' in r[1]:
+            print('{} is blocked by robots.txt, ignoring and treating as non duplicate'.format(uri))
+            return None
+        if 'Requested Line is too  large' in r[1]:
+            print('{} is too long for the API, Ignoring and treating as non duplicate'.format(uri))
+            return None
+        if not len(r[1]) == 14:
+            print('\n')
+            print('http://wwwb-dedup.us.archive.org:8083/cdx/search/'
+                    '?url={}'.format(uri) +
+                    '&limit=1'
+                    '&filter=digest:{}'.format(digest.split(':')[1]) +
+                    '&fl=original,timestamp'
+                    '&to={}'.format(int(date) - 1) +
+                    '&filter=!mimetype:warc\/revisit'
+                 )
+            print('Date received - ' + str(r[1]))
+            raise Exception('Got an invalid response from the Deduplication API - Aborting (Date out of range)' )
+        try:
+            URLValidator(str(r[0]))
+        except ValidationError as e:
+            print(e)
+            print('Expected ' + uri + " , Got " + str(r[0]))
+            raise Exception('Got an invalid response from the Deduplication API - Aborting (URL does not match)')
         return {
             'target-uri': r[0],
             'date': datetime.datetime.strptime(r[1], '%Y%m%d%H%M%S'). \
@@ -132,7 +184,6 @@ class Warc:
         if value is not None:
             self._warc_target = value
         self._warc_target = create_warc_target(self.warc_source)
-        
 
 
 def create_warc_target(warc_source: str) -> str:
@@ -140,4 +191,3 @@ def create_warc_target(warc_source: str) -> str:
         return warc_source.rsplit('.', 2)[0] + '.deduplicated.warc.gz'
     elif warc_source.endswith('.warc'):
         return warc_source.rsplit('.', 1)[0] + '.deduplicated.warc'
-
